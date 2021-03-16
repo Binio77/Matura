8d55eaf12358a0c47f88c13496394ef1cb8400b2
Artur Binczyk <binczykartur77@gmail.com>
2021-03-16 11:28:35 +0100

    Matura 2019 kwiecien zad 4.1

---
+++ Matura 2020 kwiecien/4,1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+    ifstream f_wej("dane4.txt");
+    ofstream f_wyj("zadanie4.txt");
+    int tab[1000], najwR = INT_MIN, najmR = INT_MAX;
+    for(int i=0; i<1000; i++)
+        f_wej >> tab[i];
+
+    for(int i=0; i<1000; i++)
+    {
+        int roznica = abs(tab[i] - tab[i+1]);
+        if(roznica > najwR) 
+            najwR = roznica;
+
+        if(roznica < najmR) 
+            najmR = roznica;
+    }
+
+    f_wyj << najwR << endl <<najmR;
+
+    getchar();
+    getchar();
+    return 0;
+}
