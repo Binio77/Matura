33822ffd41a8e0d3d091fd8ccd3650f5e07f4d26
Artur Binczyk <binczykartur77@gmail.com>
2021-10-18 16:17:57 +0200

    Matura 2020 lipiec

--- Matura 2020 lipiec/main.cpp
+++ Matura 2020 lipiec/main.cpp
@@ -20,7 +20,7 @@ int main()
     int najwsuma;
     for(int i = 0; i<20; i++)
     {
-        int const letterOffset = 'A' + 10;
+        int const letterOffset = 'A' - 10;
         string name, letters, numbers;
         int suma = 0;
         wej >> name;
